b62e3298c3ff30a40780f2d195f5d0bc083243fb
Michael Calder <mac@MacBook-Air.attlocal.net>
2023-07-27 13:02:26 -0500

    updates

---
+++ Exercise1.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Prints Celsius to Fahr table in reverse order w/ column headers and spacing.
+
+
+// defining the symbolic constants
+#define LOWER 0 
+#define UPPER 300
+#define STEP 20
+
+int main()
+{
+    int fahr;
+    printf("Celsius Fahrenheit\n");
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+	printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
---
+++ readchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//copy input to output; version 1
+
+int main()
+{
+   	int c;
+
+	while ((c = getchar()) != EOF) {
+		putchar(c);
+	}
+}
